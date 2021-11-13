cf2b47a6492d2e6e19de45a014012a88c8002735
xu0808 <haohaoqiankunxu@163.com>
2021-11-13 08:38:31 +0800

    局部敏感hash验证

--- work/pyspark/lsh.py
+++ work/pyspark/lsh.py
@@ -6,7 +6,7 @@
 import datetime
 from pyspark.ml.feature import BucketedRandomProjectionLSH
 from pyspark.ml.linalg import Vectors
-from pyspark.sql.functions import col
+from pyspark.sql.functions import *
 from pyspark.sql import SparkSession
 
 
@@ -27,16 +27,15 @@ if __name__ == '__main__':
         .getOrCreate()
 
     # 2、初始化数据
-    dataA = [(0, Vectors.dense([1.0, 1.0]),),
+    array = [(0, Vectors.dense([1.0, 1.0]),),
              (1, Vectors.dense([1.0, -1.0]),),
              (2, Vectors.dense([-1.0, -1.0]),),
-             (3, Vectors.dense([-1.0, 1.0]),)]
-    dfA = spark.createDataFrame(dataA, ["id", "features"])
-    dataB = [(4, Vectors.dense([1.0, 0.0]),),
+             (3, Vectors.dense([-1.0, 1.0]),),
+             (4, Vectors.dense([1.0, 0.0]),),
              (5, Vectors.dense([-1.0, 0.0]),),
              (6, Vectors.dense([0.0, 1.0]),),
              (7, Vectors.dense([0.0, -1.0]),)]
-    dfB = spark.createDataFrame(dataB, ["id", "features"])
+    df = spark.createDataFrame(array, ["id", "features"])
 
     # 输入表示欧几里得空间一个点
     # bucketLength：更大的桶降低了假阴性率
@@ -44,20 +43,21 @@ if __name__ == '__main__':
     # 另外，还有基于Jaccard距离的MinHashLSH，将每个数据集表示为一个二进制稀疏向量
     brp = BucketedRandomProjectionLSH(inputCol="features", outputCol="hashes", bucketLength=2.0,
                                       numHashTables=3)
-    model = brp.fit(dfA)
+    model = brp.fit(df)
     # Feature Transformation
     print("The hashed dataset where hashed values are stored in the column 'hashes':")
-    model.transform(dfA).show()
+    model.transform(df).show(20, False)
 
     # Compute the locality sensitive hashes for the input rows, then perform approximate similarity join.
     # We could avoid computing hashes by passing in the already-transformed dataset, e.g.
     # `model.approxSimilarityJoin(transformedA, transformedB, 1.5)`
     print("Approximately joining dfA and dfB on Euclidean distance smaller than 1.5:")  # 计算欧式距离（阈值小于1.5才纳入）
     # 2个数据集
-    model.approxSimilarityJoin(dfA, dfB, 1.5, distCol="EuclideanDistance") \
-        .select(col("datasetA.id").alias("idA"),
-                col("datasetB.id").alias("idB"),
-                col("EuclideanDistance")).show()
+    model.approxSimilarityJoin(df, df.limit(6), 5, distCol="EuclideanDistance") \
+        .select(col("datasetA.id").alias("a"),
+                col("datasetB.id").alias("b"),
+                col("EuclideanDistance").alias("d"))\
+        .orderBy("a", desc("d")).show(20, False)
 
     # Compute the locality sensitive hashes for the input rows, then perform approximate nearest neighbor search.
     # We could avoid computing hashes by passing in the already-transformed dataset, e.g.
@@ -65,6 +65,6 @@ if __name__ == '__main__':
     key = Vectors.dense([1.0, 0.0])
     print("Approximately searching dfA for 2 nearest neighbors of the key:")
     # 从dfA中找到2个与key距离最近的点
-    model.approxNearestNeighbors(dfA, key, 2).show()
+    model.approxNearestNeighbors(df, key, 2).show(20, False)
 
     spark.stop()
