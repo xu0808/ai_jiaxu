ab21e2b6ebab3fd782a786cbd1b87a79a00a4a5b
xu0808 <haohaoqiankunxu@163.com>
2021-11-20 15:35:40 +0800

    环境查看

--- doc/dev_env.py
+++ doc/dev_env.py
@@ -1,5 +1,72 @@
 import tensorflow as tf
+import sys
 
-# 检查环境，打印版本
+# 打印Python版本
+print("Python version: ", sys.version)
+print("Python version info: ", sys.version_info)
+
+
+# 检查tensorflow环境，打印版本
 print(tf.__version__)
 print(tf.test.is_gpu_available())
+
+
+# 检查tensorflow依赖清单
+# pip freeze > piplist.txt
+# absl-py==1.0.0
+# asn1crypto==1.3.0
+# astunparse==1.6.3
+# cachetools==4.2.4
+# certifi==2019.11.28
+# cffi==1.14.0
+# chardet==3.0.4
+# conda==4.8.2
+# conda-package-handling==1.6.0
+# cryptography==2.8
+# gast==0.3.3
+# google-auth==2.3.3
+# google-auth-oauthlib==0.4.6
+# google-pasta==0.2.0
+# grpcio==1.42.0
+# h5py==2.10.0
+# idna==2.8
+# importlib-metadata==4.8.2
+# Keras-Preprocessing==1.1.1
+# Markdown==3.3.6
+# menuinst==1.4.16
+# numpy==1.18.5
+# oauthlib==3.1.1
+# opt-einsum==3.3.0
+# pandas==1.3.4
+# protobuf==3.19.1
+# pyasn1==0.4.8
+# pyasn1-modules==0.2.8
+# pycosat==0.6.3
+# pycparser==2.19
+# pyOpenSSL==19.1.0
+# PySocks==1.7.1
+# python-dateutil==2.8.2
+# pytz==2021.3
+# pywin32==227
+# requests==2.22.0
+# requests-oauthlib==1.3.0
+# rsa==4.7.2
+# ruamel-yaml==0.15.87
+# scipy==1.4.1
+# six==1.14.0
+# tensorboard==2.7.0
+# tensorboard-data-server==0.6.1
+# tensorboard-plugin-wit==1.8.0
+# tensorflow-gpu==2.3.0
+# tensorflow-gpu-estimator==2.3.0
+# tensorflow-hub==0.12.0
+# termcolor==1.1.0
+# tqdm==4.42.1
+# typing-extensions==4.0.0
+# urllib3==1.25.8
+# Werkzeug==2.0.2
+# win-inet-pton==1.1.0
+# wincertstore==0.2
+# wrapt==1.13.3
+# zipp==3.6.0
+
