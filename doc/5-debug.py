37b41bf3fa3fc562f790a5ac2d28dcd2b0a57aef
xu0808 <haohaoqiankunxu@163.com>
2021-11-20 21:28:12 +0800

    电影评分hash

--- doc/5-debug.py
+++ doc/5-debug.py
@@ -6,9 +6,13 @@ import numpy as np
 data_dir = 'D:\\study\\rec_4\\data'
 tf_record_dir = 'D:\\study\\rec_4\\tf_record'
 
-# 打印Python版本
-print("Python version: ", sys.version)
-print("Python version info: ", sys.version_info)
+
+def debug():
+    # 打印Python版本
+    print("Python version: ", sys.version)
+    print("Python version info: ", sys.version_info)
+    print('hash(\'user_id=1\') =', np.int32(hash('user_id=1')))
+    print('type(hash(\'user_id=1\')) =', type(hash('user_id=1')))
 
 
 def read_rating():
@@ -20,7 +24,7 @@ def read_rating():
     # 取出数组
     rating_0 = rating_df.values.astype(np.int32)
     print('rating_0.shape:', rating_0.shape)
-    # 取出user， movie， rating
+    # 取出user_id， movie_id， rating
     rating = rating_0[:, [0, 2, 4]]
     print('rating.shape:', rating.shape)
     print('rating:', rating)
@@ -28,7 +32,16 @@ def read_rating():
     rating_1 = rating[rating[:, 0] < 2000]
     print('rating_1.shape:', rating_1.shape)
     print('rating_1:', rating_1)
+    # hash离散化
+    # 作用：a.更好的稀疏表示；b. 具有一定的特征压缩；c.能够方便模型的部署
+    rating_hash = []
+    for i in range(rating_1.shape[0]):
+        hash_line = [hash('user_id=%d' % rating_1[i, 0]), hash('movie_id=%d' % rating_1[i, 1]), rating_1[i, 2]]
+        rating_hash.append(hash_line)
+    print('rating_hash.shape: (%d, %d)' % (len(rating_hash), len(rating_hash[0])))
+    print('rating_hash last 5:', rating_hash[-5:])
 
 
 if __name__ == '__main__':
     read_rating()
+    # debug()
