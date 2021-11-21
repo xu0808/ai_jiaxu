0754770a1ef574b54cb7f34171db669dfc5261c0
xu0808 <haohaoqiankunxu@163.com>
2021-11-21 09:35:08 +0800

    完成tf_record读写工具封装

---
+++ study/rec_4/utils/reader.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# coding: utf-8
+import os
+import pandas as pd
+import numpy as np
+
+data_dir = 'D:\\study\\rec_4\\data'
+tf_record_dir = os.path.join(data_dir, 'tf_record')
+
+
+def read_rating():
+    # 数据文件
+    ratings_file = os.path.join(data_dir, 'ratings.dat')
+    # 不能直接使用'::'分割
+    rating_df = pd.read_csv(ratings_file, encoding="utf-8", header=None, sep=':')
+    print('rating_df:', rating_df)
+    # 取出数组
+    rating_0 = rating_df.values.astype(np.int32)
+    print('rating_0.shape:', rating_0.shape)
+    # 取出user_id， movie_id， rating
+    rating = rating_0[:, [0, 2, 4]]
+    print('rating.shape:', rating.shape)
+    print('rating:', rating)
+    # 只取出用户小于2000的记录
+    rating_1 = rating[rating[:, 0] < 2000]
+    print('rating_1.shape:', rating_1.shape)
+    print('rating_1:', rating_1)
+    # hash离散化
+    # 作用：a.更好的稀疏表示；b. 具有一定的特征压缩；c.能够方便模型的部署
+    rating_hash = []
+    for i in range(rating_1.shape[0]):
+        hash_line = [hash('user_id=%d' % rating_1[i, 0]), hash('movie_id=%d' % rating_1[i, 1]), rating_1[i, 2]]
+        rating_hash.append(hash_line)
+    print('rating_hash.shape: (%d, %d)' % (len(rating_hash), len(rating_hash[0])))
+    print('rating_hash last 5:', rating_hash[-5:])
+    return rating_hash
+
+
+if __name__ == '__main__':
+    read_rating()
--- study/rec_4/utils/tf_record.py
+++ study/rec_4/utils/tf_record.py
@@ -1,9 +1,10 @@
-# tfRecordFile文件写入
+#!/usr/bin/env python
+# coding: utf-8
+# tfRecordFile文件读写
+import os
 import tensorflow as tf
-
-print(tf.__version__)
-data_dir = 'D:\\study\\deepshare\\rec_4\\data'
-tf_record_dir = 'D:\\study\\deepshare\\rec_4\\tf_record'
+import numpy as np
+import reader
 
 
 def bytes_feature(value):
@@ -19,4 +20,88 @@ def float_feature(value):
 
 def int64_feature(value):
     return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
+
+
+def write(file_name, keys, types, feature_data):
+    """
+    数据写入tf_record
+    Args:
+        file_name 数据写入文件名称
+        keys 每个特征名称
+        types 每个特征类型（bytes、float、int64）
+        feature_data 特征数据（二维数组，逐行写如，每列对应key和type）
+    """
+    # 数据文件
+    tf_dir = os.path.join(reader.tf_record_dir, file_name)
+    with tf.io.TFRecordWriter(tf_dir) as writer:
+        # 需要逐行写入
+        for line in feature_data:
+            # 建立tf.train.Feature字典
+            feature = {}
+            for j in range(len(keys)):
+                feature[keys[j]] = eval('%s_feature' % types[j])(line[j])
+            # 通过字典建立 Example
+            example = tf.train.Example(features=tf.train.Features(feature=feature))
+            # 将Example序列化
+            serialized = example.SerializeToString()
+            # 写入TFRecord文件
+            writer.write(serialized)
+        writer.close()
+
+
+def read(file_name, keys, types, batch_size=20):
+    """
+    数据写入tf_record
+    Args:
+        file_name 数据文件名称
+        keys 每个特征名称
+        types 每个特征类型（如：tf.int64）
+        batch_size 批大小
+    """
+    # 定义Feature结构，告诉解码器每个Feature的类型是什么
+    feature_desc = {}
+    for i in range(len(keys)):
+        feature_desc[keys[i]] = tf.io.FixedLenFeature([], types[i])
+
+    # tf.train.Example解码,独立取出每个字段
+    def _parse_example(example_string):
+        result = tf.io.parse_single_example(example_string, feature_desc)
+        return [result[key] for key in keys]
+
+    tf_dir = os.path.join(reader.tf_record_dir, file_name)
+    tf_dataset_0 = tf.data.TFRecordDataset(tf_dir)
+    tf_dataset = tf_dataset_0.map(_parse_example)
+    tf_dataset = tf_dataset.batch(batch_size)
+    # 高效批处理
+    tf_dataset = tf_dataset.prefetch(tf.data.experimental.AUTOTUNE)
+    return tf_dataset
+
+
+def write_recod():
+    ratings_data = reader.read_rating()
+    keys = ['user_id', 'movie_id', 'rating']
+    types = ['int64', 'int64', 'int64']
+    write('rating', keys, types, ratings_data)
+
+
+def read_recod():
+    keys = ['user_id', 'movie_id', 'rating']
+    types = [tf.int64, tf.int64, tf.int64]
+    # 分批读出每个特征
+    data_set = read('rating', keys, types)
+    size = 0
+    for user_id, movie_id, rating in data_set:
+        if size == 0:
+            print('user_id = ', user_id)
+            print('movie_id = ', movie_id)
+            print('rating = ', rating)
+        size += 1
+    print('data_set size = ', size)
+
+
+if __name__ == '__main__':
+    # write_recod()
+    read_recod()
+
+
 
