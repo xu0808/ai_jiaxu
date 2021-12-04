c0e95720137786e519e11ca432d96ef013d7b960
xu0808 <haohaoqiankunxu@163.com>
2021-12-04 09:58:39 +0800

    解决梯度为None问题

--- study/rec_4/week_1/model.py
+++ study/rec_4/week_1/model.py
@@ -6,16 +6,12 @@ import params_server
 
 batch_size = 12875
 vector_dim = 16
-# 梯度下降
-num_epoch = 10000 # 迭代轮数
-# 优化器
-optimizer = tf.keras.optimizers.SGD(learning_rate=5e-4)
+
 
 def matrix_cf():
     vec_shape = [batch_size, vector_dim]
     user_id_emb = tf.random.normal(vec_shape)
     movie_id_emb = tf.random.normal(vec_shape)
-    vars = [user_id_emb, movie_id_emb]
     epochs = 30
     for epoch in range(epochs):
         print('Start of epoch %d' % (epoch,))
@@ -26,13 +22,12 @@ def matrix_cf():
         for user_id, movie_id, label in data_set:
             y_true = tf.constant(label, dtype=tf.int64)
             with tf.GradientTape() as tape:
+                tape.watch([user_id_emb, movie_id_emb])
                 y_pre = tf.reduce_mean(user_id_emb * movie_id_emb, axis=1)
-                loss = tf.losses.mean_squared_error(y_pre, y_true)
-            grads = tape.gradient(loss, vars)
-            # user_id_emb -= grads[0] * 0.05
-            # movie_id_emb -= grads[1] * 0.05
-            # 根据梯度 更新参数
-            # optimizer.apply_gradients(grads_and_vars=zip(grads, vars))
+                loss = tf.reduce_mean(tf.square(y_pre, y_true))
+            grads = tape.gradient(loss, [user_id_emb, movie_id_emb])
+            user_id_emb -= grads[0] * 0.05
+            movie_id_emb -= grads[1] * 0.05
             print('loss = ', loss.numpy())
 
 
