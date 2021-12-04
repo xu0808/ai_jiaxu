0ba2a242c7fc33b6ea419b6a34a7176fce1a3550
xu0808 <haohaoqiankunxu@163.com>
2021-12-04 21:29:27 +0800

    基于cacheout缓存完成MatrixCF模训练

--- study/rec_4/week_1/matrix_cf.py
+++ study/rec_4/week_1/matrix_cf.py
@@ -1,19 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
 # -*- 自动求导机制
 import tensorflow as tf
+import pandas as pd
+import os
 import tf_record
+import params_server
+import reader
 # 基于矩阵分解的embedding模型
 
+# 为了保证对齐，需要全量训练
 batch_size = 200
 vector_dim = 16
 learning_rate = 0.5
+epochs = 1000
 
 
 def train():
-    # 初始化向量
-    vec_shape = [batch_size, vector_dim]
-    user_id_emb = tf.random.normal(vec_shape)
-    movie_id_emb = tf.random.normal(vec_shape)
-    epochs = 30
+    ps = params_server.PS(vector_dim=vector_dim)
+    # 1、模型训练
     for epoch in range(epochs):
         print('Start of epoch %d' % (epoch,))
         keys = ['user_id', 'movie_id', 'rating']
@@ -22,6 +27,9 @@ def train():
         data_set = tf_record.read('rating', keys, types, batch_size=batch_size)
         batch_num = 0
         for user_id, movie_id, label in data_set:
+            # 初始化和读取向量
+            user_id_emb = tf.constant(ps.pull(user_id.numpy()))
+            movie_id_emb = tf.constant(ps.pull(movie_id.numpy()))
             y_true = tf.constant(label, dtype=tf.int64)
             with tf.GradientTape() as tape:
                 tape.watch([user_id_emb, movie_id_emb])
@@ -34,15 +42,31 @@ def train():
             # 最小损失推出
             if loss < 0.000001:
                 break
+                # 最小损失推出
+            # if batch_num > 2:
+            #     break
             # 梯度下降
             grads = tape.gradient(loss, [user_id_emb, movie_id_emb])
             user_id_emb -= grads[0] * learning_rate
             movie_id_emb -= grads[1] * learning_rate
+            # 更新向量
+            ps.push(user_id.numpy(), user_id_emb.numpy())
+            ps.push(movie_id.numpy(), movie_id_emb.numpy())
             batch_num += 1
 
     print('result -> epoch = %d, batch_num = %d, loss = %f' % (epoch, batch_num, loss.numpy()))
     print('user_id_emb top 5', user_id_emb[0:5])
     print('movie_id_emb top 5', movie_id_emb[0:5])
+    # 2、向量存取
+    keys, values = [], []
+    for key in ps.cache:
+        keys.append(key)
+        values.append(ps.cache.get(key))
+
+    emb_df = pd.DataFrame({'key': keys, 'vec': values})
+    # 数据文件
+    emb_file = os.path.join(reader.data_dir, 'rating_emb.csv')
+    emb_df.to_csv(emb_file, index=False, sep=',')
 
 
 if __name__ == "__main__":
--- study/rec_4/week_1/params_server.py
+++ study/rec_4/week_1/params_server.py
@@ -2,29 +2,53 @@
 # coding: utf-8
 # 参数服务器
 
+# pip install cacheout
+# 缓存库
+import cacheout
 import numpy as np
+import time
 
 
 # 单例模式
 class PS:
     def __init__(self, vector_dim):
         np.random.seed(2020)
-        self.params = dict()
+        self.cache = cacheout.Cache(maxsize=100000, ttl=0, timer=time.time, default=None)
         self.dim = vector_dim
         print("params_server inited...")
 
     def pull(self, keys):
         values = []
-        # 这里传进来的数据是[batch, feature_len]
+        cache_dict = self.cache.get_many(keys)
+        # 判断是否包含不存在的key
+        new_cache_dict = {}
         for key in keys:
-            value = self.params.get(key, None)
-            if value is None:
+            if key not in cache_dict:
+                print('value is None -> ', key)
                 # 用到的时候才随机产生
                 value = np.random.rand(self.dim)
-                self.params[key] = value
-            values.append(value)
+                cache_dict[key] = value
+                new_cache_dict[key] = value
+
+            values.append(cache_dict[key])
+        if len(new_cache_dict) > 0:
+            self.cache.set_many(new_cache_dict)
+
         return np.asarray(values, dtype='float32')
 
     def push(self, keys, values):
-        for i in range(len(keys)):
-            self.params[keys[i]] = values[i]
+        cache_dict = {k: v for (k, v) in zip(keys, values)}
+        self.cache.set_many(cache_dict)
+
+
+if __name__ == "__main__":
+    # 默认不过期
+    ps = PS(vector_dim=2)
+    ps.cache.set('-6990346318830052955', {'data': {}}, ttl=1)
+    assert ps.cache.get('-6990346318830052955') == {'data': {}}
+    time.sleep(1)
+    assert ps.cache.get('-6990346318830052955') is None
+
+    # 设置
+    ps.cache.set_many({'a': 1, 'b': 2, 'c': 3})
+    print(ps.cache.get_many(['a', 'a', 'b', 'd']))
