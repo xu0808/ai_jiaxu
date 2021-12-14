6d9e0c96dbe587f14e22d0c869950366c24fe6c3
xu0808 <haohaoqiankunxu@163.com>
2021-12-14 22:26:35 +0800

    tf2.0实现FM基础版

--- study/rec_4/week3/dnn.py
+++ study/rec_4/week3/dnn.py
@@ -23,31 +23,12 @@ types = [tf.int64, tf.int64, tf.int64, tf.int64, tf.float32]
 # 分批读出每个特征
 data_set = tf_record.read('click_log', keys, types, batch_size=batch_size)
 
-config = {
-    "train_file": "../data2/train",
-    "test_file": "../data2/val",
-    "saved_embedding": "../data4/saved_dnn_embedding",
-    "max_steps": 10000,
-    "train_log_iter": 1000,
-    "test_show_step": 1000,
-    "last_test_auc": 0.5,
-
-    "saved_checkpoint": "checkpoint",
-    "checkpoint_name": "dnn",
-
-    "saved_pb": "../data2/saved_model",
-
-    "input_tensor": ["input_tensor"],
-    "output_tensor": ["output_tensor"]
-}
-
 
 class DNN_Model(tf.keras.models.Model):
-
     def __init__(self):
         super(DNN_Model, self).__init__()
         self.d1 = tf.keras.layers.Dense(32, use_bias=False, activation=active)
-        self.d2 = tf.keras.layers.Dense(16, use_bias=False, activation=active )
+        self.d2 = tf.keras.layers.Dense(16, use_bias=False, activation=active)
         self.d3 = tf.keras.layers.Dense(1, use_bias=False, activation=None)
 
     def call(self, x):
@@ -72,14 +53,14 @@ def train():
             embs = [tf.constant(ps.pull(f.numpy())) for f in features]
             y_true = tf.reshape(label, [200, 1])
             with tf.GradientTape() as tape:
-
                 # 【所有参数计算必须位于tape.watch后】
                 tape.watch(embs)
                 # 所有特征向量拼接
                 inputs = tf.concat(embs, 1)
                 logits = model(inputs)
                 loss = tf.reduce_mean(
-                  tf.nn.sigmoid_cross_entropy_with_logits(logits=logits, labels=y_true))
+                    tf.nn.sigmoid_cross_entropy_with_logits(logits=logits, labels=y_true))
+                tf.nn.l2_loss
             # 损失计算
             if batch_num % 100 == 0:
                 print('epoch = %d, batch_num = %d, loss = %f' % (epoch, batch_num, loss.numpy()))
