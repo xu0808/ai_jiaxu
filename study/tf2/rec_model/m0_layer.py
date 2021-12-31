b96c97d1d255be8bcfe85c2fa61ec7a4c48d7c9b
xu0808 <haohaoqiankunxu@163.com>
2021-12-31 09:53:35 +0800

    抽象出通用的线性回归和多层神经网络层

---
+++ study/tf2/rec_model/m0_layer.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# coding: utf-8
+# 通用layer
+
+import tensorflow as tf
+from tensorflow.keras.layers import Layer, Dense, Embedding
+from tensorflow.keras.regularizers import l2
+from tensorflow import random_normal_initializer as init
+
+
+class Line_layer(Layer):
+    def __init__(self, feature_num):
+        super().__init__()
+        self.w0 = self.add_weight(name='w0', shape=(1,),
+                                  initializer=tf.zeros_initializer(), trainable=True)
+        self.w = self.add_weight(name='w', shape=(feature_num, 1),
+                                 initializer=init(), trainable=True, regularizer=l2(1e-4))
+
+    def call(self, inputs):
+        x = tf.matmul(inputs, self.w) + self.w0
+        return x
+
+
+class Deep_layer(Layer):
+    def __init__(self, hidden_units, output_dim, activation):
+        super().__init__()
+        self.hidden_layer = [Dense(i, activation=activation) for i in hidden_units]
+        # 输出层没有激活函数
+        self.output_layer = Dense(output_dim, activation=None)
+
+    def call(self, inputs):
+        x = inputs
+        for layer in self.hidden_layer:
+            x = layer(x)
+        output = self.output_layer(x)
+        return output
+
